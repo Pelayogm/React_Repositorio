2d32158b3670973217417f473710a07189ad2581
Pelayo García Menéndez <pelayogm@informatica.cifpaviles.pa>
2024-10-18 17:51:50 +0200

    Introduccion JS y pruebas Bootstrap 5

---
+++ IntroducciónAJavaScript/index.js
@@ -0,0 +1,11 @@
+const apellidos = ["Paez", "Perez"];
+var nombrePersona  = "Juan Pablo";
+
+function imprimirPorPantalla (var1, var2) {
+    console.log("Hola Mundo");
+    console.log("La edad es: " + var2);
+    console.log(var1);
+}
+
+imprimirPorPantalla(apellidos[0], 32);
+console.log("El individuo se llama: " + nombrePersona);
